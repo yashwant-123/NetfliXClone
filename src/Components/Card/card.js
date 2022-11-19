c6b9d0a9d84a15069308f2fcddde0f4b02d968e0
yashwant-123 <yashwanthkumar.chamakura@gmail.com>
2022-11-19 14:02:18 +0530

    ...

--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -1,14 +1,70 @@
-import React, { Component } from 'react';
-import './card.css';
+import axios from "axios";
+import React, { Component } from "react";
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+
+import "./card.css";
 
 function Card(props) {
-       const imgaeurl=`https://image.tmdb.org/t/p/original/${props.poster}`;
-        console.log(imgaeurl);
-    return ( <>
-        <div className='Card'>
-            <img className='image' src={imgaeurl} alt="poster" />
-        </div>
-    </> );
+  const imgaeurl = `https://image.tmdb.org/t/p/original/${props.poster}`;
+  const [video, setVideo] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
+  const youtubeUrl = `https://www.youtube.com/watch?v=_pAmhY0siWM`;
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const fetchURL = async () => {
+    try {
+        const response = await axios.get(`/movie/${props.id}`, {
+            params: {
+              append_to_response: 'videos',
+            },
+          });
+          if (response.data.videos.results.lenght > 0) {
+            setVideo(response.data.videos.results[0].key);
+          }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchURL();
+  }, []);
+
+  const vid =`${youtubeUrl}${video}`
+  console.log(vid);
+
+  return (
+    <>
+    
+    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    {!isHovered ? (
+        
+      <img className='image' src={imgaeurl} alt='poster' />
+    ) : (
+        
+      <ReactPlayer
+      
+      url={vid}
+      playing={true}
+      width ='300px'
+      height= '200px'
+    />
+
+
+    )
+    
+    }
+  </div>
+  </>
+);
 }
 
 export default Card;
+
--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -1,34 +1,33 @@
-import React, { useEffect } from 'react';
-import Card from '../Card/card';
-import {  useState } from 'react';
-import axios from 'axios';
-import './Row.css';
+import React, { useEffect } from "react";
+import Card from "../Card/card";
+import { useState } from "react";
+import axios from "axios";
+import "./Row.css";
 function Row(props) {
+  const [movie, setmovies] = useState([]);
+  const getmovies = async () => {
+    const api_d = await axios.get(props.url);
+    setmovies(api_d.data.results);
+  }; 
 
-    const [movie,setmovies] =useState([]);
-    const getmovies= async ()=>{
-        const api_d=await axios.get(props.url);
-        setmovies(api_d.data.results);
-        console.log(api_d);
-            
-    };
-   
 
-    useEffect(()=>{
-        getmovies();
-    },[]);
-    return (  
-        <>
-            <div className='row_wrapper'>
-                <h3 className='Title'>{props.title}</h3>
-                <div className='row_card'>
-                    {movie.map((el) => {
-                        return <Card title={el.title}  poster={el.backdrop_path} />
-                    })}
-                </div>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    getmovies();
+  }, []);
+  return (
+    <>
+      <div className="row_wrapper">
+        <h3 className="Title">{props.title}</h3>
+        <div className="row_card">
+          {movie.map((el) => {
+            return (
+              <Card title={el.title} poster={el.backdrop_path} id={el.id} />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Row;
